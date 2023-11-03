ca9c4dc0ff7975a1cbda720d870907d22db272cb
Darlan Pacheco <darlanpacheco@proton.me>
2023-11-03 01:11:02 -0300

    First day trying to use TWM

--- debianSetup.sh
+++ debianSetup.sh
@@ -1,5 +1,5 @@
 # Update the repository and the system.
-sudo apt update && sudo apt upgrade
+apt update && apt upgrade
 
 # Setting up Bash.
 echo "PS1='[\[\e[38;2;190;190;255m\]\w\[\e[0m\]]> '
@@ -20,18 +20,24 @@ alias ai='sudo apt install'
 alias ar='sudo apt remove'
 alias dr='dotnet run'" >~/.bashrc
 
+# Installing Wget.
+apt install wget
+
 # Dotnet repository.
 wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
-sudo apt update
+# Update the system again.
+apt update
 
 # Installing packages from the official Debian repository.
-sudo apt install git curl neofetch man ranger
-sudo apt install gnome-shell gnome gnome-software-plugin-flatpak gnome-tweaks gnome-boxes flatpak snapd file-roller mangohud papirus-icon-theme
+apt install neofetch man ranger
+apt install gdm3 gnome-shell gnome gnome-software-plugin-flatpak gnome-tweaks gnome-boxes
+apt install flatpak snapd file-roller mangohud papirus-icon-theme
+apt install sudo git curl wget dosfstools ntfs-3g fuse3 dotnet-sdk-7.0 nodejs npm cargo python3 python3-pip make wine wine32
+
 sudo apt install cpu-x flameshot timeshift chromium gimp inkscape blender lmms obs-studio kdenlive steam
-sudo apt install base-files sudo dosfstools ntfs-3g fuse3 dotnet-sdk-7.0 nodejs npm cargo python3 python3-pip make wine wine32
 
 # Enable Flatpak.
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -62,11 +68,11 @@ git clone https://github.com/darlanpach/HTML-and-CSS ~/Projects/HTML-and-CSS
 git clone https://github.com/darlanpach/Website02 ~/Projects/Website02
 git clone https://github.com/darlanpach/Website03 ~/Projects/Website03
 
-# Open external links.
-xdg-open https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/
-
 # Update the system again.
 sudo apt update && sudo apt upgrade && sudo apt autoremove
+
+# Open external links.
+# xdg-open https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/
 
 # .desktop example
 # [Desktop Entry]
