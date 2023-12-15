ceb2454c3a197fd79ebb0987b05dc5ef0c67496c
Darlan Pacheco <darlanpacheco@proton.me>
2023-12-15 16:19:48 -0300

    LibreOffice and CMatrix install.

--- debian-setup.sh
+++ debian-setup.sh
@@ -7,7 +7,7 @@ rm packages-microsoft-prod.deb
 
 sudo apt update
 
-sudo apt install vim neovim neofetch ranger
+sudo apt install vim neovim neofetch ranger cmatrix
 sudo apt install wget curl git dotnet-sdk-8.0 nodejs npm lua5.4 wine
 
 sudo npm install -g typescript
