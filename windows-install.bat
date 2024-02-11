94cd46ae845e7caec1b6f5cad35b277725ec7fb0
Darlan Pacheco <darlanpacheco@proton.me>
2024-02-11 09:01:28 -0300

    Adding LocalSend.

--- windows-install.bat
+++ windows-install.bat
@@ -4,7 +4,7 @@ winget install Microsoft.DotNet.SDK.8 OpenJS.NodeJS Git.Git Neovim.Neovim
 winget install GIMP.GIMP KDE.Krita BlenderFoundation.Blender LMMS.LMMS OBSProject.OBSStudio KDE.Kdenlive
 winget install Valve.Steam EpicGames.EpicGamesLauncher Roblox.Roblox
 winget install Microsoft.PowerToys Microsoft.VisualStudioCode
-winget install Mozilla.Firefox.DeveloperEdition Discord.Discord Flameshot.Flameshot GodotEngine.GodotEngine.Mono Bitwarden.Bitwarden Oracle.VirtualBox Upscayl.Upscayl TheDocumentFoundation.LibreOffice AdrienAllard.FileConverter
+winget install Mozilla.Firefox.DeveloperEdition Discord.Discord Flameshot.Flameshot GodotEngine.GodotEngine.Mono Bitwarden.Bitwarden Oracle.VirtualBox Upscayl.Upscayl TheDocumentFoundation.LibreOffice LocalSend.LocalSend AdrienAllard.FileConverter
 
 winget upgrade
 
